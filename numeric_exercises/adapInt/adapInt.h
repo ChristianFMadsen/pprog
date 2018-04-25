b34e89b10819a40440cd5583855732a26b248dd5
Christian Færgemand Madsen <madsen_1337@hotmail.com>
2018-04-25 12:14:51 +0200

    ...

--- numeric_exercises/adapInt/adapInt.h
+++ numeric_exercises/adapInt/adapInt.h
@@ -9,5 +9,5 @@ double int6(double x, int* fCalls);
 double intRoutine(double f(double x, int* fCalls),double a, double b, double acc, double eps, double x2, double x3, double* error, int numberOfRecursions, int* fCalls);
 double my_integrator(double f(double x, int* fCalls),double a,double b, double acc,double eps, double* error, int* fCalls);
 double aToInf(double f(double x, int* fCalls), double a, double acc, double eps, double* error, int* fCalls);
-double clenCurtis(double f(double x, int* fCalls), double acc, double eps, double* error, int* fCalls);
+double clenCurtis(double f(double x, int* fCalls), double a, double b, double acc, double eps, double* error, int* fCalls);
 #endif
